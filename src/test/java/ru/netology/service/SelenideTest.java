b553c26cd1a27accb62ee1ac31a2f9ecc991fda8
Regie <reginapanina@mail.ru>
2023-05-04 14:53:23 +0300

    Build success

---
+++ src/test/java/ru/netology/service/SelenideTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class SelenideTest {
+    String str = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+
+    @Test
+    void shouldSubmitFormSuccessfully() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(Keys.chord(Keys.CONTROL));
+        $("[data-test-id=name] input").setValue("Романов Кирилл");
+        $("[data-test-id=phone] input").setValue("+79134204422");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").should(appear, Duration.ofSeconds(15));
+        $("[data-test-id=date] input").setValue(str);
+
+
+    }
+
+}
