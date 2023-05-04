4a992fb375a190af61734402ca01bd74ea036e9d
Regie <reginapanina@mail.ru>
2023-05-04 19:10:02 +0300

    Check

--- src/test/java/ru/netology/service/SelenideTest.java
+++ src/test/java/ru/netology/service/SelenideTest.java
@@ -15,24 +15,19 @@ import static com.codeborne.selenide.Selenide.*;
 class SelenideTest {
     String str = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-
     @Test
-    void shouldSubmitFormSuccessfully() {
+    void shouldSubmitForm() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
         $("[data-test-id=date] input").setValue(str);
-        //$("[data-test-id=date] input").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         $("[data-test-id=name] input").setValue("Романов Кирилл");
         $("[data-test-id=phone] input").setValue("+79134204422");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).waitUntil(visible, 15000);
-        $("[data-test-id=notification] .notification__content").shouldHave(text("Встреча успешно забронирована на " + str), Duration.ofSeconds(15));
+        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + str), Duration.ofSeconds(15));
     }
-
-    //$("[data-test-id=notification]").shouldHave( text("Встреча успешно забронирована на " + str), Duration.ofSeconds(15));
-
 
 }
