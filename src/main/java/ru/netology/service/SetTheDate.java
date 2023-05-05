3a5506e2d8c572bf1e4201a1aeebaebb5ab9a286
Regie <reginapanina@mail.ru>
2023-05-05 11:19:37 +0300

    New strategy

---
+++ src/main/java/ru/netology/service/SetTheDate.java
@@ -0,0 +1,12 @@
+package ru.netology.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class SetTheDate {
+    //public String generateDate(long addDays, String pattern) {
+    //    return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+//}
--- src/test/java/ru/netology/service/SelenideTest.java
+++ src/test/java/ru/netology/service/SelenideTest.java
@@ -13,21 +13,27 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 class SelenideTest {
-    String str = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @Test
     void shouldSubmitForm() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        $("[data-test-id=date] input").setValue(str);
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Романов Кирилл");
         $("[data-test-id=phone] input").setValue("+79134204422");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).waitUntil(visible, 15000);
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + str), Duration.ofSeconds(15));
-    }
+        $("[data-test-id=notification]")
+                .shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
 
+    }
 }
+
